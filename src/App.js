a94f146329ba115849de6b16cac970f62e9d0b1d
jessicaUP <uphoff.jessica@gmail.com>
2022-06-06 12:44:09 -0400

    Create searchPhotos component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import './App.css';
+import { createClient } from 'pexels';
+
+const client = createClient('563492ad6f91700001000001166f44fea82146a5ad74cdf9f30f0569');
+
+// All requests made with the client will be authenticated
+
 
 function App() {
   return (
