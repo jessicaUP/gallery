a8216a8b9e8faa822b6e5c69c36a6e4b749d66d5
jessicaUP <uphoff.jessica@gmail.com>
2022-06-06 14:46:53 -0400

    Add search images"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
 import React from 'react';
-// import { createClient } from 'pexels';
+
 import SearchPhotos from './components/searchPhotos';
 
-// const client = createClient('563492ad6f91700001000001166f44fea82146a5ad74cdf9f30f0569');
 
-// // All requests made with the client will be authenticated
 
 
 function App() {
--- src/components/searchPhotos.jsx
+++ src/components/searchPhotos.jsx
@@ -1,14 +1,32 @@
 import React, { useState } from "react";
+import { createClient } from 'pexels';
+
+
 
 export default function SearchPhotos() {
   const [ query, setQuery ] = useState("")
+  const [ photos, setPhotos ] = useState([])
 
-  console.log(query);
-  
+  const client = createClient('563492ad6f91700001000001166f44fea82146a5ad74cdf9f30f0569');
+
+
+  const searchPhotos = (e) => {
+    e.preventDefault();
+    client.photos.search({ query, per_page: 10 }).then(photos => { setPhotos(photos.photos) });
+
+    console.log(photos)
+
+  }
+  console.log(photos)
+  const photoList = photos.map((photo) =>
+    <div className="tile" key={photo.id}>
+      <img src={photo.src.small} alt={photo.alt} />
+    </div>)
+
   return (
     <>
     <h1>SEARCH TEST</h1>
-    <form action="">
+    <form className="form" onSubmit={searchPhotos}>
       <label htmlFor="query">SEARCH</label>
       <input
         type="text"
@@ -22,6 +40,10 @@ export default function SearchPhotos() {
         SEARCH
       </button>
     </form>
+
+    <div className="photos">
+      { photoList }
+    </div>
     </>
   );
 }
