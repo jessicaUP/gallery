39576fe2a84f89f6a22dbef492702391cf0b3a31
jessicaUP <uphoff.jessica@gmail.com>
2022-06-06 13:43:37 -0400

    create form

--- src/components/searchPhotos.jsx
+++ src/components/searchPhotos.jsx
@@ -4,6 +4,18 @@ export default function SearchPhotos() {
   return (
     <>
     <h1>SEARCH TEST</h1>
+    <form action="">
+      <label htmlFor="query">SEARCH</label>
+      <input
+        type="text"
+        name="query"
+        className="input"
+        placeholder="Search Photos"
+      />
+      <button type='submit' className="button">
+        SEARCH
+      </button>
+    </form>
     </>
   );
 }
