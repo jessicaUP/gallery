e9e422d0a89d721a584e7efacd02da8aebc22e49
jessicaUP <uphoff.jessica@gmail.com>
2022-06-07 13:36:47 -0400

    Fix up SearchPhotos

--- src/components/searchPhotos.jsx
+++ src/components/searchPhotos.jsx
@@ -2,22 +2,34 @@ import React, { useState } from "react";
 import { createClient } from 'pexels';
 
 
-
 export default function SearchPhotos() {
-  const [ query, setQuery ] = useState("")
-  const [ photos, setPhotos ] = useState([])
+  const [ typed, setTyped ] = useState('');
+  const [ query, setQuery ] = useState("cats");
+  const [ photos, setPhotos ] = useState([]);
+  const [ pageNum, setPageNum ] = useState(1);
 
   const client = createClient('563492ad6f91700001000001166f44fea82146a5ad74cdf9f30f0569');
 
+  async function fetchPhotos() {
+    client.photos.search({ query, page: pageNum, per_page: 10 }).then(photos => {
+      setPhotos(photos.photos);
+    });
+    // const fullResponse = await fetch(`https://api.pexels.com/v1/search/?page=${pageNum}&per_page=10&query=${query}`);
+    // const responseJson = await fullResponse.json();
+
+    // console.log(fullResponse, responseJson);
+    // setPhotos(responseJson.photos);
+  }
+
+  React.useEffect(() => {
+    fetchPhotos();
+  }, [ query, pageNum ]);
 
   const searchPhotos = (e) => {
     e.preventDefault();
-    client.photos.search({ query, per_page: 10 }).then(photos => { setPhotos(photos.photos) });
-
-    console.log(photos)
-
+    setQuery(typed);
   }
-  console.log(photos)
+
   const photoList = photos.map((photo) =>
     <div className="tile" key={photo.id}>
       <img src={photo.src.small} alt={photo.alt} />
@@ -33,8 +45,8 @@ export default function SearchPhotos() {
         name="query"
         className="input"
         placeholder="Search Photos"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
+        value={typed}
+        onChange={(e) => setTyped(e.target.value)}
       />
       <button type='submit' className="button">
         SEARCH
